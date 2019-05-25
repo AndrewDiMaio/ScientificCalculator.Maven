02e0790fcd88f6149508c94e5921f39953d6baa9
Benjamin Corbett <benjamincorbett@zipcodersMBP37.localdomain>
2019-05-25 13:36:36 -0400

    updated code

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -22,14 +22,62 @@ public class Console {
             System.out.println("Please choose the Calculator you would like to access.");
             menuInput = scan.nextInt();
 
-            if (menuInput == 1){
-
+            calculatorSelection: if (menuInput == 1){
                 simpleCalc.BasicCalc();
+                int operator = scan.nextInt();
+                if (operator == 5) {
+                    break calculatorSelection;
+                }
+
+
+                    System.out.println("Please enter the first number. ");
+                    double x = scan.nextDouble();
+
+                    System.out.println("Please enter the second number");
+                    double y = scan.nextDouble();
+
+
+                    switch (operator){
+
+                        case 1:
+                            double answer = simpleCalc.getAddition(x, y);
+                            System.out.println(x + " + " + y + " = " + answer);
+                            break;
+
+                        case 2:
+                            answer = simpleCalc.getSubtraction(x, y);
+                            System.out.println(x + " - " + y + " = " + answer);
+                            break;
+
+                        case 3:
+                            answer = simpleCalc.getMultiplication(x, y);
+                            System.out.println(x + " * " + y + " = " + answer);
+                            break;
+
+                        case 4:
+                            if (y == 0) {
+                                System.out.println(x + " / " + y + " = Undefined");
+                            }
+                            else {
+                                answer = simpleCalc.getDivision(x, y);
+                                System.out.println(x + " / " + y + " = " + answer);
+                            }
+                            break;
+
+
+
+                    }
+
+                }
+
+
+                }
+
             }
-            
+
 
 
 
         }
-    }
-}
+
+
--- src/main/java/com/zipcodewilmington/scientificcalculator/SimpleCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/SimpleCalc.java
@@ -1,28 +1,58 @@
 package com.zipcodewilmington.scientificcalculator;
 
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SimpleCalc {
     double x;
     double y;
     double answer;
-    int operator;
+    //int operator;
 
-        public void BasicCalc(){
-            Scanner scan= new Scanner(System.in);
-            System.out.println("Would you like to: " + "\n 1)" +
-                    " Add" + "\n 2) Subtract" + "\n 3) Mulitply" + "\n 4) Divide");
-            operator = scan.nextInt();
+    public void BasicCalc() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to: " + "\n 1)" +
+                " Add" + "\n 2) Subtract" + "\n 3) Mulitply" + "\n 4) Divide" + "\n 5) Exit");
+
+    }
+
+
+        /*if (operator == 5){
+            return;
+            }
+        else {
 
             System.out.println("Please enter the first number. ");
             x = scan.nextDouble();
 
             System.out.println("Please enter the second number");
             y = scan.nextDouble();
+*/
+        public double getAddition(double x, double y){
+            answer = x + y;
+            return answer;
+        }
 
-            switch (operator) {
+        public double getSubtraction(double x, double y) {
+            answer = x - y;
+            return answer;
+        }
+
+        public double getMultiplication(double x, double y) {
+            answer = x * y;
+            return answer;
+        }
+
+        public double getDivision(double x, double y) {
+            answer = x / y;
+            return answer;
+        }
+
+
+
+
+
+
+            /*switch (operator) {
                 case 1:
                     answer = x + y;
                     System.out.println(x + " + " + y + " = " + answer);
@@ -36,18 +66,20 @@ public class SimpleCalc {
                     System.out.println(x + " * " + y + " = " + answer);
                     break;
                 case 4:
-                    answer = x/y;
-                    if (y == 0){
+                    answer = x / y;
+                    if (y == 0) {
                         System.out.println("You can't divide by zero.  Try again.");
-                    }
-                    else {
+                    } else {
                         System.out.println(x + " / " + y + " = " + answer);
                     }
                     break;
 
+             */
             }
 
-        }
 
 
-    }
+
+
+
+
