c5e87acf6e55ec0d43fde739771051154d34fa30
andrewdimaio <andrewdimaio777@gmail.com>
2019-05-25 15:25:32 -0400

    advanced calc changed inverse sign

--- src/main/java/com/zipcodewilmington/scientificcalculator/AdvancedCalc.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/AdvancedCalc.java
@@ -6,10 +6,10 @@ public class AdvancedCalc {
     double y;
     double answer;
 
-    public void AdvancedCalc() {
+    public void AdvanCalc() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Would you like to: " + "\n 1) x2)" + "\n 2) √x)" + "\n 3) x^y" + "\n 4) 1/x" + "\n 5) Invert");
-
+    }
         public double getSquared(double x) {
                 answer = Math.pow(x,2);
                 return answer;
@@ -17,6 +17,7 @@ public class AdvancedCalc {
 
         public double getSquareRoot(double x) {
                 answer = Math.sqrt(x);
+
                 return answer;
         }
 
@@ -31,8 +32,13 @@ public class AdvancedCalc {
         }
 
         public double getInvertSign(double x) {
-                answer = x;
-                break;
+                if (x < 0) {
+                    answer = Math.abs(x);
+                }else if (x > 0) {
+                    answer = x * (-1);
+                }else{
+                    answer = 0;
+                return answer;
         }
 
     }
